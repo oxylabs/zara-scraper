3483fab412c369ae6fbdfa44de924a6f6c4e19ea
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:37:15 +0200

    Adding code examples/zara_nodejs.js

---
+++ code examples/zara_nodejs.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+const body = {
+  'source': 'universal',
+  'url': 'https://www.zara.com/ww/en/woman-blazers-l1055.html?v1=2290737'};
+  
+const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+  }
+});
+
+console.log(await response.json());
