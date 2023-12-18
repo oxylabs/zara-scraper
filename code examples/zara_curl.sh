f3149963f437db4559e2d5c88727c70d943c8320
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:37:22 +0200

    Adding code examples/zara_curl.sh

---
+++ code examples/zara_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.zara.com/ww/en/woman-blazers-l1055.html?v1=2290737"}'
