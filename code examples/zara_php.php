252839eeecd1d613086a42b66d7ed2b6f7b38821
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:37:19 +0200

    Adding code examples/zara_php.php

---
+++ code examples/zara_php.php
@@ -0,0 +1,26 @@
+<?php
+
+$params = [
+    'source' => 'universal',
+    'url' => 'https://www.zara.com/ww/en/woman-blazers-l1055.html?v1=2290737'];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "user" . ":" . "pass1");
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
